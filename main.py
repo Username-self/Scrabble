a03f3d8742311e2e73948f75f405d49b20b429fb
pbsinclair42 <bobsmith0030+git2@gmail.com>
2020-08-01 12:35:27 +0100

    Formatting

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from constants import LETTER_DISTRIBUTION
 from random import shuffle
 
-class Scrabble():
+
+class Scrabble:
     def __init__(self):
         self.bag = Scrabble.shake_the_bag()
 
+    @staticmethod
     def shake_the_bag():
         starting_bag = []
         for letter, count in LETTER_DISTRIBUTION.items():
